5ef92219e577e92e45ab1c1fd97db7f5f28979bb
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-12-29 01:29:46 -0600

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./face-detector/pages/video-player-page/video-player-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/app/face-detector/pages/video-player-page/video-player-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-player-page',
+  imports: [],
+  templateUrl: './video-player-page.component.html',
+  styleUrl: './video-player-page.component.scss'
+})
+export default class VideoPlayerPageComponent {}
